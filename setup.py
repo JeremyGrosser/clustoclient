93e09dab67ea14348fff55993fdb1acd53cf3b69
Jeremy Grosser <jeremy@synack.me>
2013-01-29 02:18:45 -0800

    Version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='clustoclient',
-      version='0.1',
+      version='0.2',
       py_modules=['clustohttp'],
       scripts=['clusto-template'],
       install_requires=['jinja2', 'IPy'])
